75b1c6bf593ed52831dbd43bdac3f055e79a6cb7
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2024-11-27 19:37:38 -0300

    changes

--- src/http.ts
+++ src/http.ts
@@ -6,6 +6,20 @@ import { Server } from 'socket.io';
 const app = express();
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
+app.get('/healthz', (req, res) => {
+    const healthCheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: new Date(),
+    };
+
+    try {
+        res.status(200).json(healthCheck);
+    } catch (error) {
+        res.status(503).json({ message: 'Service Unavailable' }); 
+    }
+});
+
 const server = http.createServer(app)
 const io = new Server(server)
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { server } from "./http"
 import './websocket'
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`http://localhost:${port}`)
